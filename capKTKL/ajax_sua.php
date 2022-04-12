4686b5f86604ff87dc57e6d2b787aa1320d88fd5
khaivn1996 <103101719+khaivn1996@users.noreply.github.com>
2022-04-12 10:14:12 +0700

    Delete capKTKL directory

--- capKTKL/ajax_sua.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include('../dbconnect.php');
-?>
-<?php 
-	$result = mysqli_query($con,"UPDATE CAPKTKL SET TenCapKTKL='".$_POST['TenCapKTKLm']."' WHERE MaCapKTKL='".$_POST['MaCapKTKLm']."'");
-?>
