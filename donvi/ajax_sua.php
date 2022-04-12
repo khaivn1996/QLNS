90f8f9b1d793812e2892bbac8bf13cd68a7ddf2b
khaivn1996 <103101719+khaivn1996@users.noreply.github.com>
2022-04-12 10:14:40 +0700

    Delete donvi directory

--- donvi/ajax_sua.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include('../dbconnect.php');
-?>
-<?php 
-	$result = mysqli_query($con,"UPDATE DONVI SET TenDV='".$_POST['TenDVm']."',SoDT='".$_POST['SoDTm']."' WHERE MaDV='".$_POST['MaDVm']."'");
-?>
