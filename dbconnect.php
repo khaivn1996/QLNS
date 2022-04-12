e6eeb3214fef90ecdfdd0ba28c21b6bd292fd501
khaivn1996 <khaiit.tybach@gmail.com>
2022-04-12 10:09:12 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 $con = mysqli_connect("localhost","root","","QLNS");
-
+mysqli_set_charset($con, 'UTF8');
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
