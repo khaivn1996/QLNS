74809dd73dea472dc44a0307398978fa17b4eff3
khaivn1996 <103101719+khaivn1996@users.noreply.github.com>
2022-04-12 10:14:28 +0700

    Delete danhmucKTKL directory

--- danhmucKTKL/ajax_sua.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include('../dbconnect.php');
-?>
-<?php 
-	$result = mysqli_query($con,"UPDATE DMKTKL SET TenKTKL='".$_POST['TenKTKLm']."',KTKL='".$_POST['KTKLm']."' WHERE MaKTKL='".$_POST['MaKTKLm']."'");
-?>
